afbe05fded677128fb4f380e785633439a42aa02
Ximena Contreras <satelliteg@gmail.com>
2019-06-01 00:10:51 -0400

    Build Test 1

---
+++ precache-manifest.19db6989168a60b7eca1d01a7ad92ac8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3045cbd16f31715583166b7f2238a156",
+    "url": "/groupon-test/index.html"
+  },
+  {
+    "revision": "5a1d5f3344901461b397",
+    "url": "/groupon-test/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "5b537948f24afc95a481",
+    "url": "/groupon-test/static/js/2.5dbd8ed4.chunk.js"
+  },
+  {
+    "revision": "5a1d5f3344901461b397",
+    "url": "/groupon-test/static/js/main.da75060e.chunk.js"
+  },
+  {
+    "revision": "c04a8a4d3c2c354f2d05",
+    "url": "/groupon-test/static/js/runtime~main.14565708.js"
+  }
+]);
